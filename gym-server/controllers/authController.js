dfab7415d17cafde0e78a18ec667de3a67cef930
FRVR-JAE <tjoloyede9@gmail.com>
2023-10-08 01:01:48 -0700

    updated

--- gym-server/controllers/authController.js
+++ gym-server/controllers/authController.js
@@ -1,19 +1,105 @@
 const User = require('../models/User');
 
 const registerUser = async (req, res) => {
-  // Implementation of registerUser function
+  try {
+    const { username, password } = req.body;
+
+    // Check if the user already exists
+    const existingUser = await User.findOne({ username });
+    if (existingUser) {
+      return res.status(400).json({ message: 'User already exists' });
+    }
+
+    // Create a new user
+    const newUser = new User({ username, password });
+    await newUser.save();
+
+    return res.status(200).json({ message: 'User registered successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 const loginUser = async (req, res) => {
-  // Implementation of loginUser function
+  try {
+    const { username, password } = req.body;
+
+    // Check if the user exists
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: 'User not found' });
+    }
+
+    // Check if the password is correct
+    const isPasswordValid = await user.comparePassword(password);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    // Generate a JWT token for authentication
+    const token = generateToken(user);
+
+    return res.status(200).json({ message: 'User logged in successfully', token });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 const resetPassword = async (req, res) => {
-  // Implementation of resetPassword function
+  try {
+    const { username, newPassword } = req.body;
+
+    // Find the user
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: 'User not found' });
+    }
+
+    // Update the user's password
+    user.password = newPassword;
+    await user.save();
+
+    return res.status(200).json({ message: 'Password reset successful' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
 };
 
 const changePassword = async (req, res) => {
-  // Implementation of changePassword function
+  try {
+    const { username, currentPassword, newPassword } = req.body;
+
+    // Find the user
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(400).json({ message: 'User not found' });
+    }
+
+    // Check if the current password is correct
+    const isPasswordValid = await user.comparePassword(currentPassword);
+    if (!isPasswordValid) {
+      return res.status(401).json({ message: 'Invalid password' });
+    }
+
+    // Update the user's password
+    user.password = newPassword;
+    await user.save();
+
+    return res.status(200).json({ message: 'Password changed successfully' });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
+const generateToken = (user) => {
+  // Generate a JWT token with user information
+  // You can use a library like jsonwebtoken to generate the token
+  const token = jwt.sign({ userId: user._id, username: user.username }, 'secretKey', { expiresIn: '1h' });
+  return token;
 };
 
 module.exports = { registerUser, loginUser, resetPassword, changePassword };
