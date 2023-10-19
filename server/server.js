3316a75d87012c769bc286eaf70675c5e8a78867
MhykelB <MhykelB>
2023-10-19 01:07:18 +0100

    fxed

--- server/server.js
+++ server/server.js
@@ -1,28 +1,33 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // Import controllers
-const likesController = require('./controllers/likesController');
-const videoController = require('./server/controllers/videoController');
-const usersController = require('./server/controllers/usersController');
+const likesController = require("./controllers/likesController");
+const videoController = require("./controllers/videosController");
+const usersController = require("./controllers/usersController");
 
 // Import models
-const Like = require('./server/models/likes');
-const Video = require('./server/models/video');
-const User = require('./server/models/user');
+const Like = require("./models/like");
+const Video = require("./models/video");
+const User = require("./models/user");
+
+// // Import routes
+// const likesRoutes = require("./routes/likesRoutes");
+// const videoRoutes = require("./routes/videoRoutes");
+// const usersRoutes = require("./routes/usersRoutes");
 
 // Import routes
-const likesRoutes = require('./server/routes/likesRoutes');
-const videoRoutes = require('./server/routes/videoRoutes');
-const usersRoutes = require('./server/routes/usersRoutes');
+const likesRoutes = require("./routes/likes"); // there is no file named likesRoutes.js in the routesfolder
+const videoRoutes = require("./routes/videos"); // there is no file named videoRoutes.js in the routesfolder
+const usersRoutes = require("./routes/users"); // there is no file named usersRoutes.js in the routesfolder
 
 // Set up middleware
 app.use(express.json());
 
 // Set up routes
-app.use('/likes', likesRoutes);
-app.use('/videos', videoRoutes);
-app.use('/users', usersRoutes);
+app.use("/likes", likesRoutes);
+app.use("/videos", videoRoutes);
+app.use("/users", usersRoutes);
 
 // Start the server
 const port = 3000;
